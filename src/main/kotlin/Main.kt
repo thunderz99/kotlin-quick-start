823cd8e2108a330975d30146cae3b6e88d3ab8e4
Lei Zhang <thunderz99@gmail.com>
2020-06-26 12:43:31 +0900

    test kotlin attach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>) {
 
 	println("""System.getenv("MESSAGE"): ${System.getenv("MESSAGE")} """)
+
+	println("Begin sleep for 10 seconds...")
+	Thread.sleep(10000)
 	println("Hello world")
 }
